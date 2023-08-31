8ef79db630cb598e22f862d3ad42ff6b0ec96732
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-08-30 22:17:21 -0500

    Laboratorio

--- src/add.js
+++ src/add.js
@@ -1,1 +1,16 @@
 import chalk from "chalk";
+
+export const add = (numero1, numero2) => numero1 + numero2
+
+export const showResult = (numero1, numero2) => {
+    
+    const result = add (numero1, numero2);
+    
+    if (result >= 5){ 
+        return chalk.green(result);
+    } else {
+        return chalk.red(result);
+    }
+}
+
+console.log(showResult(8, -3))
